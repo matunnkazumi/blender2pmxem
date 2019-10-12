e81cd4c6fa3375a1dad3925387eb6c1c1ce50c7a
matunnkazumi <developer@matunnkazumi.space>
2019-10-12 15:59:58 +0900

    バージョン番号更新

--- __init__.py
+++ __init__.py
@@ -22,7 +22,7 @@ import bpy
 bl_info = {
     "name": "MMD PMX Format (Extend)",
     "author": "matunnkazumi",
-    "version": (0, 1, 0),
+    "version": (0, 1, 2),
     "blender": (2, 80, 0),
     "location": "File > Import-Export",
     "description": "Import-Export PMX model data",
