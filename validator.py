5f00506fdc646391464f746ee19dbe0df7c6cf3c
matunnkazumi <developer@matunnkazumi.space>
2019-10-13 16:37:20 +0900

    メッセージ修正

--- validator.py
+++ validator.py
@@ -14,15 +14,15 @@ def validate_pmx(pmx_data: pmx.Model) -> List[str]:
 
     bone_name_list = [bone.Name for bone in pmx_data.Bones]
     if len(bone_name_list) != len(set(bone_name_list)):
-        result.append('Bone name must be unique.')
+        result.append('Bone name must be unique in PMX.')
 
     rigid_name_list = [rigid.Name for rigid in pmx_data.Rigids]
     if len(rigid_name_list) != len(set(rigid_name_list)):
-        result.append('Rigid name must be unique.')
+        result.append('Rigid name must be unique in PMX.')
 
     joint_name_list = [joint.Name for joint in pmx_data.Joints]
     if len(joint_name_list) != len(set(joint_name_list)):
-        result.append('Joint name must be unique.')
+        result.append('Joint name must be unique in PMX.')
 
     return result
 
@@ -32,14 +32,14 @@ def validate_xml(xml_root) -> List[str]:
 
     bone_name_list = [e.get("name") for e in xml_root.findall("bones/bone")]
     if len(bone_name_list) != len(set(bone_name_list)):
-        result.append('Bone name must be unique.')
+        result.append('Bone name must be unique in XML.')
 
     rigid_name_list = [e.get("name") for e in xml_root.findall("rigid_bodies/rigid")]
     if len(rigid_name_list) != len(set(rigid_name_list)):
-        result.append('Rigid name must be unique.')
+        result.append('Rigid name must be unique in XML.')
 
     joint_name_list = [e.get("name") for e in xml_root.findall("constraints/constraint")]
     if len(joint_name_list) != len(set(joint_name_list)):
-        result.append('Joint name must be unique.')
+        result.append('Joint name must be unique in XML.')
 
     return result
