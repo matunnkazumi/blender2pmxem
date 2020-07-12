9f8b868d9dd62f0547abf016f8ad1dfe9a345bb1
matunnkazumi <developer@matunnkazumi.space>
2020-07-12 23:28:36 +0900

    まず簡単に長さだけチェック

--- tests/test_pmx.py
+++ tests/test_pmx.py
@@ -1,4 +1,5 @@
 import unittest
+from pathlib import Path
 
 from pmx import pmx
 
@@ -8,3 +9,21 @@ class TestPmx(unittest.TestCase):
     def test_Model(self):
         model = pmx.Model()
         self.assertIsNotNone(model)
+
+    def test_load_model(self):
+        test_pmx = Path(__file__).parent / 'data' / 'test_01.pmx'
+
+        model = pmx.Model()
+        with test_pmx.open(mode="rb") as f:
+            model.Load(f)
+
+        self.assertEqual(len(model.Vertices), 14)
+        self.assertEqual(len(model.Faces), 12*3)
+        self.assertEqual(len(model.Textures), 0)
+        self.assertEqual(len(model.Materials), 1)
+        self.assertEqual(len(model.Bones), 1)
+        self.assertEqual(len(model.Morphs), 1)
+        self.assertEqual(len(model.DisplayFrames), 3)
+        self.assertEqual(len(model.Rigids), 1)
+        self.assertEqual(len(model.Joints), 2)
+        self.assertEqual(len(model.SoftBodies), 0)
